bd5da0703f6071adbeb6f0155274058639b5db8a
xiuazo <xiuset@gmail.com>
2025-07-02 14:52:33 +0200

    cleanup

--- scripts/eta_check.py
+++ scripts/eta_check.py
@@ -7,44 +7,40 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 dotenv_path = os.path.join(script_dir, '.env')
 load_dotenv(dotenv_path, override=True)
 
-AUTO_PAUSE = False
-AUTO_RESUME = False
+AUTO_PAUSE = True
+AUTO_RESUME = True
 
-QB_URL = os.getenv('QBITTORRENT_URL')
-QB_USER = os.getenv('QBITTORRENT_USERNAME')
-QB_PASSWORD = os.getenv('QBITTORRENT_PASSWORD')
+os.environ['QBITTORRENTAPI_HOST'] = os.getenv('QBITTORRENT_URL')
+os.environ['QBITTORRENTAPI_USERNAME'] = os.getenv('QBITTORRENT_USERNAME')
+os.environ['QBITTORRENTAPI_PASSWORD'] = os.getenv('QBITTORRENT_PASSWORD')
 
 def completed(torrent):
-    maxtime = torrent['max_seeding_time'] * 60 # API dice que en segundos. falso. devuelve minutos
-    return maxtime >= 0 and maxtime < t['seeding_time']
+    maxtime = torrent.max_seeding_time * 60 # API dice que en segundos. falso. devuelve minutos
+    return maxtime >= 0 and maxtime < torrent.seeding_time
 
 if __name__ == "__main__":
-    conn_info = dict({
-        'host': QB_URL,
-        'username': QB_USER,
-        'password': QB_PASSWORD
-    })
-    with qbittorrentapi.Client(**conn_info) as qbt_client:
+    with qbittorrentapi.Client() as qbt_client:
         torrents = qbt_client.torrents_info()
 
-        pause, resume = set(), set()
+        pause, resume = list(), list()
         for t in torrents:
             paused = t.state == 'pausedUP'
             if completed(t):
                 if not paused and t.state != 'forcedUP':
-                    # print(f"Warning: {t['name']} is beyond his sharelimits")
-                    pause.add(t.hash)
+                    pause.append(t)
             elif paused:
-                    # print(f"Warning: {t['name']} stopped and not reached sharelimit")
-                    resume.add(t.hash)
+                    resume.append(t)
 
-        print('Should pause\n' + '-'*60)
+        print('Should be paused:\n' + '-'*40)
         if pause:
-            print(pause)
+            for t in pause:
+                print(f"{t['name']} ({t.tracker[8:20]}..) is beyond his sharelimits")
             if AUTO_PAUSE:
-                qbt_client.torrents_stop(pause)
-        print('Should resume\n' + '-'*60)
+                qbt_client.torrents_stop({t['hash'] for t in pause})
+        print('\nShould be active:\n' + '-'*40)
         if resume:
-            print(resume)
+            for t in resume:
+                print(f"{t['name']} ({t.tracker[8:20]}..) not reached sharelimit")
             if AUTO_RESUME:
-                qbt_client.torrents_start(resume)
+                pass
+                qbt_client.torrents_start({t['hash'] for t in resume})
